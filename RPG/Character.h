8c6fde8d6a500088d92da0b6ccc48a962ad22392
Fernando <fernandogutierrezjr0@gmail.com>
2023-07-26 14:36:17 -0700

    Fixing punctuation errors

--- RPG/Character.h
+++ RPG/Character.h
@@ -4,24 +4,24 @@ class Character {
 public:
 
 	/*
-	* @brief Default Constructor to initalize data members
+	* @brief Default Constructor to initalize data members.
 	*/
 	Character() = default;
 
 	/*
 	* @brief Constructor to initalize data members
 	* @params health The health of the character.
-	* @params attackPower The attack power of the character
-	* @params defense The defense level of the character
+	* @params attackPower The attack power of the character.
+	* @params defense The defense level of the character.
 	*/
 	Character(int health, int attackPower, int defense);
 
 	/*
-	* @brief Default Destructor
+	* @brief Default Destructor.
 	*/
 	virtual ~Character() = default;
 
-	// Pure Virtual functions for common character actions
+	// Pure Virtual functions for common character actions.
 	virtual void attack(Character& target) = 0;
 	virtual void defend(int incomingDage) = 0;
 	virtual void useAbility() = 0;
@@ -30,20 +30,20 @@ public:
 	virtual void displayInfo() const = 0;
 
 	/*
-	* @breif Sets the health of the character
-	* @param health The health assigned to the character
+	* @breif Sets the health of the character.
+	* @param health The health assigned to the character.
 	*/
 	void setHealth(int health);
 
 	/*
-	* @breif Sets the attack power of the character
-	* @param attackPower The attack power assigned to the character
+	* @breif Sets the attack power of the character.
+	* @param attackPower The attack power assigned to the character.
 	*/
 	void setAttackPower(int attackPower);
 
 	/*
-	* @breif Sets the defense level of the character
-	* @param defense The defense level assigned to the character
+	* @breif Sets the defense level of the character.
+	* @param defense The defense level assigned to the character.
 	*/
 	void setDefenseLevel(int defense);
 
