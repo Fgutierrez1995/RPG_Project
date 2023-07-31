794b22d22b7cb3d637a611037c8a59cc6457008f
Fernando <fernandogutierrezjr0@gmail.com>
2023-07-31 14:56:52 -0700

    Fixed some of the comments to a single line.

--- RPG/Mage.h
+++ RPG/Mage.h
@@ -18,9 +18,7 @@ public:
 	*/
 	Mage(int attackPower, int defense, const std::string& name);
 
-	/*
-	* @brief Default Destructor.
-	*/
+	/** @brief Default Destructor. */
 	~Mage() = default;
 
 	/*
@@ -41,19 +39,13 @@ public:
 	*/
 	virtual void heal(int amount) override;
 
-	/*
-	* @brief Method is used to level up the mage object. This method increased the mage attack, defense, combat, health level.
-	*/
+	/** @brief Method is used to level up the mage object. This method increased the mage attack, defense, combat, health level. */
 	virtual void levelUp() override;
 
-	/*
-	* @brief Method to check if the mage is dead (health is zero or less)
-	*/
+	/** @brief Method to check if the mage is dead (health is zero or less). */
 	virtual bool isDead() const override;
 
-	/*
-	* @brief Method to display all the mage levels.
-	*/
+	/** @brief Method to display all the mage levels. */
 	virtual void displayInfo() const override;
 
 private:
