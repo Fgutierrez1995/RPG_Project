3b2c5b36dd53258835f2c43cfbfabb32642ec7cf
Fernando <fernandogutierrezjr0@gmail.com>
2023-07-31 15:00:41 -0700

    Created a Character Creation Class that creates Warrior, Mage and Rogue objects.

---
+++ RPG/CharacterCreation.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Warrior.h"
+#include "Mage.h"
+#include "Rogue.h"
+#include <memory>
+
+class CharacterCreation {
+public:
+	/** @brief This method is used to create characters in the RPG game using the 3 provided classes (e.g Warrior, Mage or Rogue). */
+	static std::unique_ptr<Character> createCharacter();
+
+private:
+
+	/** @brief creates a unique_ptr Warrior object. */
+	static std::unique_ptr<Warrior> createWarrior();
+
+	/** @brief creates a unique_ptr Warrior object. */
+	static std::unique_ptr<Mage> createMage();
+
+	/** @brief creates a unique_ptr Warrior object. */
+	static std::unique_ptr<Rogue> createRogue();
+
+};
