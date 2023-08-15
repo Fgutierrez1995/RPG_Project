159952d4de23da97bb4e0534b11fd827df280e02
Fernando <fernandogutierrezjr0@gmail.com>
2023-08-15 10:27:28 -0700

    Using main to test combat simulation.

--- RPG/main.cpp
+++ RPG/main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
-#include "Warrior.h"
+#include "WeaponItem.h"
+#include "ArmourItem.h"
+#include "ConsumableItem.h"
+#include "CharacterCreation.h"
+#include "CombatSystem.h"
+#include "Orc.h"
+#include "DarkMage.h"
+#include "Goblin.h"
+
 
 int main() {
 
-	Warrior samurai(100, 5, 45, 50);
-	samurai.displayInfo();
+	auto warrior = CharacterCreation::createCharacter();
+	auto orc = Orc::makeOrc(50, 20, 100, "Orc");
+	auto darkMage = DarkMage::makeDarkMage(20, 0, 75, "Dark Mage");
+	auto goblin = Goblin::makeGoblin(18, 2, 50, "Goblin");
+
+	warrior->addItem(std::move(WeaponItem::makeWeapon("Long Sword", "Great sword", 50)));
+	warrior->addItem(std::move(ArmourItem::makeArmour("Black Armour", "Excellent Armour.", 30)));
+	warrior->addItem(std::move(ConsumableItem::makeConsumable("Shark", "Excellent cooked shark.", 1000)));
+
+	CombatSystem combat(warrior, goblin);
+
+	combat.startBattle();
 
 	return 0;
 }
