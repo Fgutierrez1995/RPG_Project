f806664785a77ac329bc3bd0fa517da9c6845cb2
Fernando <fernandogutierrezjr0@gmail.com>
2023-08-17 11:03:16 -0700

    updated main to call the Adventure class and simulate a characters first quest line.

--- RPG/main.cpp
+++ RPG/main.cpp
@@ -1,28 +1,14 @@
 #include <iostream>
-#include "WeaponItem.h"
-#include "ArmourItem.h"
-#include "ConsumableItem.h"
-#include "CharacterCreation.h"
-#include "CombatSystem.h"
-#include "Orc.h"
-#include "DarkMage.h"
-#include "Goblin.h"
+#include "Adventure.h"
 
 
 int main() {
 
-	auto warrior = CharacterCreation::createCharacter();
-	auto orc = Orc::makeOrc(50, 20, 100, "Orc");
-	auto darkMage = DarkMage::makeDarkMage(20, 0, 75, "Dark Mage");
-	auto goblin = Goblin::makeGoblin(18, 2, 50, "Goblin");
+	auto character = CharacterCreation::createCharacter();
 
-	warrior->addItem(std::move(WeaponItem::makeWeapon("Long Sword", "Great sword", 50)));
-	warrior->addItem(std::move(ArmourItem::makeArmour("Black Armour", "Excellent Armour.", 30)));
-	warrior->addItem(std::move(ConsumableItem::makeConsumable("Shark", "Excellent cooked shark.", 1000)));
+	Adventure adventure(character);
 
-	CombatSystem combat(warrior, goblin);
-
-	combat.startBattle();
+	adventure.startAdventure();
 
 	return 0;
 }
